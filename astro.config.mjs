1a0e0652a5eea9e1c11b2af81c0f36ef9fa10f66
Joel Buenrostro <joelbuenrostro@outlook.com>
2026-04-30 16:22:34 -0600

    Cambio de Host
    Arreglo con nombre de host equivocado para publicación en gh pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://joelbuenrostro.github.io',
+  site: 'https://joelbuenrostro.me',
   base: '/tech-layoffs-2026',
   output: 'static',
 });
