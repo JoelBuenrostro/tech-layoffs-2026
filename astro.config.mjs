f894811d2ae7491be4d27af663bb19fb725b06d8
Joel Buenrostro <joelbuenrostro@outlook.com>
2026-05-01 12:51:34 -0600

    Actualizacion de configuracion
    - Cambio de host al original, el host no había sido usado desde hace tiempo y no estaba activo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://joelbuenrostro.me',
+  site: 'https://joelbuenrostro.github.io',
   base: '/tech-layoffs-2026',
   output: 'static',
 });
